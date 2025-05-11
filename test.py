d9d4580ac498253c62406f254ce21885d7ff0a1c
Thiago Passos <passos.thiago2001@gmail.com>
2025-05-11 20:45:34 -0300

    gitignore folder

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import geopandas as gpd
+import shapely
 
 if __name__=="__main__":
     CRS = "EPSG:31984"
-    file_path = r"C:\Users\User\Desktop\Repositórios Locais\vertical-road-signs-design-XML\data\image\gpkg\M01-S01-CE-350-1-BRUTO-FOTO.gpkg"
+    datetime_column = "Timestamp"
+    file_path = "data/image/gpkg/M01-S01-CE-350-1-BRUTO-FOTO.gpkg"
     
     gdf = gpd.read_file(file_path).to_crs(CRS)
+    gdf = gdf.sort_values(by=datetime_column)
+    
+    line_string = shapely.LineString(list(gdf["geometry"].apply(lambda value:list(value.coords)[0])))
+    gdf_line = gpd.GeoDataFrame({"SENTIDO":["CRESCENTE"]},geometry=[line_string],crs=CRS)
 
-    print(gdf)
+    gdf_line.to_file("data/image/gpkg/M01-S01-CE-350-1-BRUTO-FOTO-LINHA.gpkg",driver="GPKG",index=False)
+    print("'-'")
